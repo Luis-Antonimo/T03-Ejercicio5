8b8c21ada4bde911ff0b7fc04e50d391d984ecb4
Luis Antónimo <luisantonimo@gmail.com>
2022-07-10 23:25:34 +0200

    ok

--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+namespace T03Ejercicio5
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a = 10;
+            int b = 20;
+            int c = 30;
+            int d = 40;
+            Console.WriteLine("Las variables originales son A:{0} B:{1} C:{2} D:{3}", a, b, c, d);
+            d = b;
+            b = c;
+            c = a;
+            a = d;
+            Console.WriteLine("Las variables modificadas son A:{0} B:{1} C:{2} D:{3}", a, b, c, d);
+        }
+    }
+}
